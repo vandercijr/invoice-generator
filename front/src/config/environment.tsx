d0d77b408d4e50b743244a0b69ec2bb143a3a439
Vanderci <vanderci@pi.tech>
2025-05-15 12:33:14 -0300

    Change env

--- front/src/config/environment.tsx
+++ front/src/config/environment.tsx
@@ -3,7 +3,7 @@ const environment = {
     baseURL: "http://localhost:8000/api",
   },
   production: {
-    baseURL: "http://localhost:8000/api",
+    baseURL: "http://api.invoicegen.space/api",
   },
 };
 
